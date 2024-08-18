081745c00ca561192af8b3f21f48b32c68913052
vishalanilpatil01 <vishalanilpatil01@gmail.com>
2024-08-18 20:17:13 +0530

    Create fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("Fizz-Buzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+// Example usage:
+const n = 20;  // You can set this to any positive integer
+fizzBuzz(n);
